294ed06074c6a80f8bb14f2e3cdf4253415cbd37
Pedro Monteiro <110275925+PedroMJK@users.noreply.github.com>
2024-10-27 09:56:32 -0300

    Conteúdo inicial adicionadp

---
+++ cypress/integration/CAC-TAT.spec.js
@@ -0,0 +1,25 @@
+{/* <reference types="Cypress" />   */}
+
+// Inicia uma suite de testes para a "Central de Atendimento ao Cliente TAT"
+describe('Central de atendimento ao Cliente TAT', function() {
+    beforeEach(function() {
+        // Para acessar a página principal da aplicação
+        cy.visit('./src/index.html')
+    }) 
+
+    it('verifica o título da aplicação', function () {
+         // Para verificar se o título da página é exatamente "Central de Atendimento ao Cliente TAT"
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+
+    it.only('preenche os campos obrigatórios e envia o formulário', function() {
+        
+        cy.get('#firstName').type('Pedro')
+        cy.get('#lastName').type('Monteiro')
+        cy.get('#email').type('pedro@exemplo.com')
+        cy.get('#open-text-area').type('Teste')
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.success').should('be.visible')
+    })
+}) 
