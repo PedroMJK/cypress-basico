4abff420e9b07bafa5954f863b03a0b8cbbe9589
Pedro Monteiro <110275925+PedroMJK@users.noreply.github.com>
2024-10-27 12:44:21 -0300

    test: adiciona casos de teste para validação de formulário
    - Adiciona testes para envio bem-sucedido do formulário com campos preenchidos corretamente
    - Adiciona testes para exibir mensagem de erro com email inválido
    - Adiciona testes para verificar o campo de telefone com entrada não numérica
    - Testa funcionalidade de limpar campos com `clear()`
    - Adiciona teste para validar formulário sem preenchimento de campos obrigatórios
    - Usa comando customizado para preencher e enviar o formulário com sucesso
    - Identifica botão de envio usando `cy.contains`
    
    Refatora testes para garantir robustez na validação de mensagens de sucesso e erro

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -12,7 +12,20 @@ describe('Central de atendimento ao Cliente TAT', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
 
-    it.only('exibe mensagem de erro ao submeter o formulário com um email com formato inválido', function() {
+    // Teste 01
+    it('preenche os campos obrigatórios e envia o formulário', function() {
+        const longText = 'Texto longo, Texto longo, Texto longo, Texto longo, Texto longo, Texto longo, Texto longo, Texto longo, Texto longo, Texto longo, Texto longo, Texto longo.'
+        cy.get('#firstName').type('Pedro')
+        cy.get('#lastName').type('Monteiro')
+        cy.get('#email').type('pedro@exemplo.com')
+        cy.get('#open-text-area').type(longText, {delay: 0})
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.success').should('be.visible')
+    })
+
+    //Teste 02
+    it('exibe mensagem de erro ao submeter o formulário com um email com formato inválido', function() {
         cy.get('#firstName').type('Pedro')
         cy.get('#lastName').type('Monteiro')
         cy.get('#email').type('pedro@exemplo,com')   // Digita um email inválido no campo com ID "email"
@@ -24,11 +37,79 @@ describe('Central de atendimento ao Cliente TAT', function() {
 
     })
 
-    // Testando o campo numérico
-    it.only('campo telefone continua vazio quando preenchido com valor não-numérico', function() {
+     // Testando o campo numérico
+    it('campo telefone continua vazio quando preenchido com valor não-numérico', function() {
         cy.get('#phone')
           .type('abcdefghijk')
           .should('have.value', '')
+    })
+    
+    // Teste 03
+    it('exibe mensagem de erro ao submeter o formulário com um email com formato inválido', function() {
+        cy.get('#firstName').type('Pedro')
+        cy.get('#lastName').type('Monteiro')
+        cy.get('#email').type('pedro@exemplo.com')
+        cy.get('#phone-checkbox').click() // Checa a funcionalidade do checkbox
+        cy.get('#open-text-area').type('Teste')
+        cy.get('button[type="submit"]').click()
+
+        // Verifica se o elemento com a classe "error" está visível, indicando um erro
+        cy.get('.error').should('be.visible')
+
+    })
+
+    // Teste 04 
+    //Usando o clear() para limpar o campo
+    it('preenche e limpa os campos nome, sobrenome, email e telefone', function() {
+        cy.get('#firstName')
+          .type('Pedro')
+          .should('have.value', 'Pedro')
+          .clear()
+          .should('have.value', '')
+        cy.get('#firstName')
+          .type('Monteiro')
+          .should('have.value', 'Monteiro')
+          .clear()
+          .should('have.value', '')
+        cy.get('#email')
+          .type('pedro@exemplo.com')
+          .should('have.value', 'pedro@exemplo.com')
+          .clear()
+          .should('have.value', '')
+        cy.get('#phone')
+          .type('1234567890')
+          .should('have.value', '1234567890')
+          .clear()
+          .should('have.value', '')
+    })
+
+    // Teste 05
+    it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios',  function () {
+        cy.get('button[type="submit"]').click()
+
+        // Verifica se o elemento com a classe "error" está visível, indicando um erro
+        cy.get('.error').should('be.visible')
+    })
+
+    // Teste 06
+    // Usando comandos customizados
+    it('envia o formulário com sucesso usando um comando customizado', function() {
+        cy.fillMandatoryFieldsAndSubmit()
+
+        // Verifica se o elemento com a classe "error" está visível, indicando um erro
+        cy.get('.success').should('be.visible')
+    })
+
+    // Teste 07
+    it.only('identifica o botão de Enviar usando a  funcionalidade contains', function() {
+        cy.get('#firstName').type('Pedro')
+        cy.get('#lastName').type('Monteiro')
+        cy.get('#email').type('pedro@exemplo.com')
+        cy.get('#open-text-area').type('Teste')
+        cy.contains('button', 'Enviar').click()
+
+        // Verifica se o elemento com a classe "error" está visível, indicando um erro
+        cy.get('.success').should('be.visible')
     })
     
 }) 
